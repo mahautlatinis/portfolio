783d48401a91160964259e32018eaa1c16f63757
mahautlatinis <mahautlatinisheritier@gmail.com>
2025-01-28 17:56:19 +0100

    init react intl

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
-import Navbar from "./components/Navbar";
 import "./index.css";
+import * as React from "react";
+import { IntlProvider } from "react-intl";
+import Navbar from "./components/Navbar";
+
+import messages_en from "./translations/en.json";
+import messages_fr from "./translations/fr.json";
 
 function App() {
+  const messages = {
+    en: messages_en,
+    fr: messages_fr,
+  };
+
+  // get browser language without the region code
+  const language = navigator.language.split(/[-_]/)[0];
+
   return (
-    <>
+    <IntlProvider
+      defaultLocale="en"
+      locale={navigator.language}
+      messages={messages[language]}
+    >
       <Navbar />
-    </>
+    </IntlProvider>
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,21 @@
+import { FormattedMessage } from "react-intl";
+import { useState } from "react";
+
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  // TODO: fixer le dropdown
+  // TODO: revoir les notions
+
   return (
     <nav className="bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
-          <img
-            src="https://flowbite.com/docs/images/logo.svg"
-            className="h-8"
-            alt="Flowbite Logo"
-          />
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-            Flowbite
+            Portfolio
           </span>
         </a>
         <button
@@ -17,9 +23,10 @@ const Navbar = () => {
           type="button"
           className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
           aria-controls="navbar-dropdown"
-          aria-expanded="false"
+          aria-expanded={isOpen ? "true" : "false"}
+          id="navbar-toggler"
+          onClick={toggleMenu}
         >
-          <span className="sr-only">Open main menu</span>
           <svg
             className="w-5 h-5"
             aria-hidden="true"
@@ -36,15 +43,19 @@ const Navbar = () => {
             />
           </svg>
         </button>
-        <div className="hidden w-full md:block md:w-auto" id="navbar-dropdown">
+        <div
+          id="navbar-dropdown"
+          className={`${isOpen ? "block" : "hidden"} w-full md:block md:w-auto`}
+        >
           <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
               <a
-                href="#"
+                href="/about"
                 className="block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent"
-                aria-current="page"
+                aria-current="home" // A revoir
               >
-                Home
+                <FormattedMessage id="about" defaultMessage="À propos" />
+                {/* A revoir */}
               </a>
             </li>
             <li>
@@ -53,7 +64,10 @@ const Navbar = () => {
                 data-dropdown-toggle="dropdownNavbar"
                 className="flex items-center justify-between w-full py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent"
               >
-                Dropdown{" "}
+                {/* <FormattedMessage
+                    id="achievements"
+                    defaultMessage="Réalisations"
+                  /> */}
                 <svg
                   className="w-2.5 h-2.5 ms-2.5"
                   aria-hidden="true"
